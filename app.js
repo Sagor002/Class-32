eab3ab2d429f64fe9f2de2bd78f4183e9199a226
Sagor002 <136358823+Sagor002@users.noreply.github.com>
2023-12-31 20:37:35 +0600

    Add files via upload
    All done

---
+++ app.js
@@ -0,0 +1,108 @@
+"use strict";
+
+// api = aplication programing interfetch
+// browser api
+// third-party api
+// ajax
+// fetch api
+// req. types --->GET/POST/PUT/DELETE --->CRUD
+
+// function fetchData() {
+//   renderLoading(true);
+
+//   fetch("https://jsonplaceholder.typicode.com/posts")
+//   .then((res) => {
+//     if (!res.ok) throw new Error("Failed to fetch");
+//   return res.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//     (renderData(data))
+//     // renderLoading(false)
+//   })
+//   .catch((err) => {
+//     console.log(err.message);
+//     renderError(err.message);
+//     // renderLoading = false
+//   }) 
+//   .finally(() => {
+//     renderLoading(false);
+//   });
+// }
+// function renderData(posts) {
+//   posts.forEach(post=> {
+//     const template =`
+//     <div>
+//       <h1>${post.id} - ${post.title}</h1>
+//     </div>
+//     `;
+//     document.body.insertAdjacentHTML("beforeend", template)
+//   });
+// }
+
+// function renderError(msg) {
+//   const template = `<p>${msg}</p>`;
+//   document.body.insertAdjacentHTML("beforeend", template);
+// }
+
+// function renderLoading(loadingState) {
+//   const template = `<h1 class="loading">Loading...</h1>`;
+
+//   if(loadingState) {
+//     document.body.insertAdjacentHTML("beforeend", template);
+//   }else{
+//     document.querySelector(".loading").style.display = "none";
+//   }
+// }
+
+// fetchData();
+// */
+
+// async function fetchData() {
+//   try {
+//     renderLoading(true)
+//     const res = await fetch("https://restcountries.com/v3.1/all");
+//     if (!res.ok) throw new Error("Failed to fetch");
+//     const data = await res.json();
+//     console.log(data);
+//     renderData(data)
+//   } catch(err) {
+//     console.log(err.message);
+//     renderError(ree.message);
+//   }finally{
+//     renderLoading(false)
+//   }
+// }
+
+// function renderData(countries) {
+//   countries.forEach((country) => {
+//     const template = `
+//       <div>
+//         <h1>Country Name: ${country?.name?.common}</h1>
+//         <p>Population: ${country.population > 1000000 ? (country?.population / 1000000).toFixed(2): country?.population}${country?.population > 1000000 ? "M" : "null"}</p>
+//         <p>Capital: ${country?.capital?.at(0)}</p>
+//         <p>Area: ${country?.area} km<sup>2</sup></p>
+//       </div>
+//     `;
+
+//     document.body.insertAdjacentHTML("beforeend", template);
+//   });
+// }
+
+// function renderError(msg) {
+//   const template = `<p>${msg}</p>`;
+  
+//   document.body.insertAdjacentHTML("beforeend", template);
+// }
+// function renderLoading(loadingState) {
+//   const template = `<h1 class="loading">Loading...</h1>`;
+
+//   if(loadingState) {
+//     document.body.insertAdjacentHTML("beforeend", template);
+//   }else{
+//     document.querySelector(".loading").style.display = "none";
+//   }
+// }
+
+// fetchData();
+
